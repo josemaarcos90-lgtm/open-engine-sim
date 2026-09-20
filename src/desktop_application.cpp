4bd0bed7a3d930fdf29041e620a0aa0a6d46e178
josemaarcos90-lgtm <jose.maarcos90@gmail.com>
2026-09-20 14:26:25 -0300

    Streamline Android user engine hotload path

--- src/desktop_application.cpp
+++ src/desktop_application.cpp
@@ -581,7 +581,7 @@ bool EngineSimApplication::loadScript(const std::string &relativeScriptPath) {
         return engine != nullptr && vehicle != nullptr && transmission != nullptr;
     };
 
-    bool loaded = compileEntry(scriptPath);
+    // Packaged main.mr is an entry point. Imported engines are catalog-style modules:\n    // compiling/executing them directly creates no objects and adds avoidable allocation churn.\n    const bool isUserEngine = relativeScriptPath.rfind("engines/user/", 0) == 0;\n    bool loaded = isUserEngine ? false : compileEntry(scriptPath);
     std::filesystem::path generatedEntryPoint;
     if (!loaded && relativeScriptPath != "main.mr") {
 #if defined(__ANDROID__)
@@ -612,9 +612,9 @@ bool EngineSimApplication::loadScript(const std::string &relativeScriptPath) {
 #endif
         loadEngine(engine, vehicle, transmission);
 #if defined(__ANDROID__)
-        checkpointAndroidMr(mrTrace.str() + "\nCHECKPOINT: loadEngine() CONCLUIDO");
-        const std::string mrLogPath = saveAndroidMrLog(mrTrace.str() + "\nRESULTADO: CARREGADO COM SUCESSO\n");
-        showAndroidMrDiagnostics(mrTrace.str() + "\nRESULTADO: CARREGADO COM SUCESSO\nLOG: " + mrLogPath);
+        checkpointAndroidMr(mrTrace.str() + "\nCHECKPOINT: loadEngine() CONCLUIDO; aguardando primeiro frame seguro");
+        const std::string mrLogPath = saveAndroidMrLog(mrTrace.str() + "\nLOAD ENGINE: CONCLUIDO\n");
+        showAndroidMrDiagnostics(mrTrace.str() + "\nLOAD ENGINE: CONCLUIDO\nLOG: " + mrLogPath);
 #endif
         return true;
     }
