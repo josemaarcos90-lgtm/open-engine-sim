71dfd976dedfe613a7c85ffcc4e70e97a337c91e
josemaarcos90-lgtm <jose.maarcos90@gmail.com>
2026-09-20 10:29:50 -0300

    Track Android external engine picker state

--- include/engine_sim_application.h
+++ include/engine_sim_application.h
@@ -202,6 +202,8 @@ class EngineSimApplication {
         std::uint64_t m_lastRenderTick = 0;
 #if defined(__ANDROID__)
         std::uint64_t m_lastPerfReportTick = 0;
+        std::uint64_t m_lastEnginePickerPollTick = 0;
+        bool m_externalEnginePickerPending = false;
         float m_lastProcessMs = 0.0f;
         float m_lastRenderMs = 0.0f;
 #endif
