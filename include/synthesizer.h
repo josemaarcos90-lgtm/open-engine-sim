e8f8e7cb4ec1f7c6a7846c0a29a00b46db494c45
M2RCOS <jose.maarcos90@gmail.com>
2026-09-21 09:26:45 -0300

    Audio: add persistent Android output limiter state

--- include/synthesizer.h
+++ include/synthesizer.h
@@ -123,6 +123,7 @@ class Synthesizer {
         RingBuffer<int16_t> m_audioBuffer;
         int m_audioBufferSize;
         int16_t *m_renderScratch = nullptr;
+        float m_outputLimiterEnvelope = 1.0f;
 
         float m_inputSampleRate;
         float m_audioSampleRate;
